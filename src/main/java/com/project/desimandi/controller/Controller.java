41db69cd5cbc260e45e50048a7a7a251fde045e1
Ravi Raj <rochak.ravi@gmail.com>
2024-09-26 11:02:51 +0530

    updating compiler option

---
+++ src/main/java/com/project/desimandi/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.project.desimandi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+    @GetMapping("/abc")
+    public String getName(){
+        return "test";
+    }
+}
